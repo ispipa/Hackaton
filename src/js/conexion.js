707ada63c66e3a9b56fb4fc1bb376697a3c612a2
Sergio-Fernandez-Dev <sr.sergiofernandezfernandez@gmail.com>
2022-07-20 18:18:21 +0200

    styling cards from innerHtml

--- src/js/conexion.js
+++ src/js/conexion.js
@@ -1,27 +1,53 @@
 import { formulario } from "./app.js";
 
-export async function response()
-{
-    window.addEventListener('load', () => {
-        fetch('../data.json')
-        .then(response => response.json())
-        .then(data => {
-            cargarDatos(data);
-        })
-    })
+export async function response() {
+  window.addEventListener("load", () => {
+    fetch("../data.json")
+      .then((response) => response.json())
+      .then((data) => {
+        cargarDatos(data);
+      });
+  });
 }
-export const cargarDatos = (data)=>
-{
+export const cargarDatos = (data) => {
+  resultado.innerHTML = "";
+  const texto = formulario.value.toLowerCase();
 
-     resultado.innerHTML='';
-     const texto = formulario.value.toLowerCase();
-
-     for (let i = 0; i < Object.values(data).length; i++) 
-     {
-     
-        if((Object.values(data)[i]["nombre"].indexOf(texto)!== -1))
-        {
-            resultado.innerHTML+='<div class="col">'+'<div class="card h-100">'+'<img src='+Object.values(data)[i]["url"]+' class="card-img-top" alt="...">'+'<div class="card-body">'+'<h5 class="card-title">'+Object.values(data)[i]["nombre"]+'</h5>'+'<p class="card-text">'+Object.values(data)[i]["descripcion"]+ '</p>'+'</div>'+'</div>'+'</div>';
-        }
-     }
-}
+  for (let i = 0; i < Object.values(data).length; i++) {
+    if (Object.values(data)[i]["nombre"].indexOf(texto) !== -1) {
+      resultado.innerHTML += `
+                <div class="col">
+                    <div class="card personalized-card-size">
+                        <img src='${
+                          Object.values(data)[i]["url"]
+                        }' alt="..." style="heigth: 120px;">
+                        <div class="card-body">
+                            <h5 class="card-title">${
+                              Object.values(data)[i]["nombre"]
+                            }</h5>
+                            <p class="card-text">
+                                <span class="bold">Rareza: </span>${
+                                  Object.values(data)[i]["rareza"]
+                                }
+                            </p>
+                            <p class="card-text">
+                                <span class="bold">Número: </span>${
+                                  Object.values(data)[i]["numero"]
+                                }
+                            </p>
+                            <p class="card-text">
+                                <span class="bold">Precio: </span>${
+                                  Object.values(data)[i]["precio"]
+                                }
+                            </p>
+                        </div>
+                        <div class="view-more-button">
+                            <a href="#"><span class="material-symbols-outlined">
+                            visibility
+                            </span><p class="corner"> Ver más</p></a>
+                        </div>
+                    </div>
+                </div>`;
+    }
+  }
+};
