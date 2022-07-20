e9737600d61f78f4cdcb27881a0f80dd7488398c
ispipa <ispipa123@gmail.com>
2022-07-20 17:09:43 +0200

    cards

--- src/js/conexion.js
+++ src/js/conexion.js
@@ -15,11 +15,10 @@ export const cargarDatos = (data)=>
 
      resultado.innerHTML='';
      const texto = formulario.value.toLowerCase();
-
      for (let i = 0; i < Object.values(data).length; i++) 
      {
-     
-        if((Object.values(data)[i]["nombre"].indexOf(texto)!== -1))
+        console.log(Object.values(data)[i]["nombre"]);
+        if(Object.values(data)[i]["nombre"].indexOf(texto)!== -1)
         {
             resultado.innerHTML+='<div class="col">'+'<div class="card h-100">'+'<img src='+Object.values(data)[i]["url"]+' class="card-img-top" alt="...">'+'<div class="card-body">'+'<h5 class="card-title">'+Object.values(data)[i]["nombre"]+'</h5>'+'<p class="card-text">'+Object.values(data)[i]["descripcion"]+ '</p>'+'</div>'+'</div>'+'</div>';
         }
